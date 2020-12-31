359b38150b4b90a655c11f4e83cdbe82ab5935cf
Neh-Sharma <bablinaneh@gmail.com>
2021-01-01 03:40:54 +0530

    added local storage persistence

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ const snippetsSection = document.querySelector("#snippets")
 const newSnippetEl = document.querySelector("#new-snippet")
 const snippetTemplate = document.querySelector("#snippet-template")
 
+const LOCAL_STORAGE_KEY = "copyPastaSnippets"
+
+let snippets = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || []
+snippets.forEach(({text, id}) => displaySnippet(createSnippetElement(text, id)))
+
 document.addEventListener("click", handleClick)
 
 function handleClick(e) {
     if(e.target.closest(".copy-button")) {
         copyToClipboard(e.target.closest(".snippet").querySelector(".snippet-text").innerText);
     } else if (e.target.closest("#save-snippet-button")) {
-        addNewSnippet(newSnippetEl.textContent)
+        addNewSnippet(newSnippetEl.value)
+    } else if (e.target.closest(".delete-button")) {
+        deleteSnippet(e.target.closest(".snippet"))
     }
 }
 
@@ -16,8 +23,31 @@ function copyToClipboard(text) {
     navigator.clipboard.writeText(text);
 }
 
-function addNewSnippet(text) {
+function addNewSnippet (text) {
+    const id = Date.now().toString()
+    saveSnippetObject(text, id)
+    const snippet = createSnippetElement(text, id)
+    displaySnippet(snippet)
+}
+
+function saveSnippetObject (text, id) {
+    snippets.push({ text, id });
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(snippets))
+}
+
+function createSnippetElement (text, id) {
     const snippet = snippetTemplate.content.cloneNode(true)
     snippet.querySelector(".snippet-text").innerText = text
+    snippet.querySelector(".snippet").id = id
+    return snippet
+}
+
+function displaySnippet (snippet) {
     snippetsSection.appendChild(snippet)
+}
+
+function deleteSnippet (snippetEl) {
+    snippets = snippets.filter(snippet => snippet.id!==snippetEl.id)
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(snippets))
+    snippetEl.remove()
 }
